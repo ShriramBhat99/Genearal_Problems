fa0ddc5deea7c761c6739d0232346e8d0f7310ac
ShriramBhat <bhatshriram9@gmail.com>
2023-01-30 22:25:04 +0530

    Second commit

---
+++ NumberSystemConversion.c
@@ -0,0 +1,44 @@
+#include<stdi.h>>
+int main() {
+
+  int num,ch;
+  printf("Enter a decimal number:");
+  scanf("%d",&num);
+  printf("1.Binary\n2.Octal\n3.Hexadecimal\n");
+  printf("Enter your choice:");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+    case 1:
+              printf("Binary equivalent is:");
+              func(num,2);
+              break;
+    case 2:
+              printf("Octal equivalent is:");
+              func(num,8);
+              break;
+    case 3:
+              printf("Hexadecimal equivalent is:");
+              func(num,16);
+              break;
+  }
+  printf("\n");
+  return 0;
+}
+
+void func(int num,int b)
+{
+ int i=0,j,rem;
+  char arr[20];
+  while(num>0)
+  {
+    rem=num%b;
+    num /=b;
+    if(rem>9 && rem<16)                   // when base is 16 their is a chance that reminder will come between 9 to 16
+              arr[i++]=rem-10+'A';        // Converting into ASCII value
+    else
+              arr[i++]=rem+'0';           // Converting into ASCII value
+  }
+  for(j=i-1;j>=0;j--)
+           printf("%c",arr[j]);
+}
